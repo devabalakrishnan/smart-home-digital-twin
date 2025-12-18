ada156704f994e40a8820f25526fd92b3f4fdda6
devabalakrishnan <deva.kathir2008@gmail.com>
2025-12-19 04:39:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-     import streamlit as st
+import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
@@ -84,3 +84,4 @@ with col2:
 # 7. AUTOMATED REFRESH (Simulates Live Data Flow)
 time.sleep(2)
 st.rerun()
+
